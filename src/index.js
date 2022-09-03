f46e20fbf619a8a003b5dc4429ff4f169fdc6d94
David <davojh27@gmail.com>
2022-09-03 11:12:16 -0500

    Se termino el desarrollo de la aplicion y se hizo el build de produccion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-
-import { saludar } from './js/componentes';
 import './styles.css';
 
-const nombre = 'Fernando';
+import { Todo, TodoList } from './classes'
+import { crearTodoHtml } from './js/componentes';
 
-saludar( nombre );
+export const todoList = new TodoList;
+
+//-----------------------------------
+
+//  renderizamos los todos que se encuentran en el localStorage
+todoList.todos.forEach(todo => crearTodoHtml( todo ));
+
+//tambien se puede hacer asi:
+// todoList.todos.forEach(crearTodoHtml);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,16 +1,114 @@
 import '../css/componentes.css'
-// import webpacklogo from '../assets/img/webpack-logo.png'
 
-export const saludar = ( nombre ) => {
-    console.log('Creando etiqueta h1');
+import { Todo } from '../classes';
+import { todoList } from '../index' 
+//REferencias en el HTMl
 
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${nombre}!!!`;
+const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
+const btnBorrar = document.querySelector('.clear-completed');
+const ulFiltros =  document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
 
-    document.body.append( h1 );
+export const crearTodoHtml = ( todo ) => {
 
-    //image
-    // const img = document.createElement('img');
-    // img.src = webpacklogo;
-    // document.body.append( img );
+    const crearHtml = `
+        <li class="${ (todo.completado) ? 'completed' : '' }" data-id="${todo.id}">
+            <div class="view">
+                <input class="toggle" type="checkbox" ${ (todo.completado) ? 'checked' : '' }>
+                <label>${ todo.tarea }</label>
+                <button class="destroy"></button>
+            </div>
+        <input class="edit" value="Create a TodoMVC template">
+        </li>
+    `;
+
+    const div = document.createElement('div');
+    div.innerHTML  = crearHtml;
+
+    divTodoList.append( div.firstElementChild );
+
+    return div.firstElementChild;
 }
+
+//Eventos
+
+txtInput.addEventListener( 'keyup', ( event )=> {
+
+    if(event.key === 'Enter' &&  txtInput.value.length >= 3){
+        const nTodo = new Todo( txtInput.value );
+        todoList.nuevoTodo( nTodo );
+        crearTodoHtml( nTodo );
+        txtInput.value = '';
+        
+    }
+
+} );
+
+divTodoList.addEventListener( 'click' , ( event ) => {
+    
+    const nameElemnt = event.target.localName;
+    const liElement = event.target.parentElement.parentElement;
+    const idElement = liElement.getAttribute('data-id')
+
+    if(nameElemnt.includes( 'input' )){
+
+        todoList.marcarCompletado( idElement );
+        liElement.classList.toggle( 'completed' );
+
+    }else if( nameElemnt.includes( 'button' ) ){
+
+        todoList.eliminarTodo( idElement );
+        divTodoList.removeChild( liElement );
+
+    }
+
+} )
+
+
+btnBorrar.addEventListener( 'click' , () => {
+    todoList.eliminarCompletados();
+    
+    for( let i = divTodoList.children.length - 1; i >= 0; i--  ){
+        const element = divTodoList.children[i];
+        if( element.classList.contains( 'completed' ) ){
+            divTodoList.removeChild(element);
+        }
+    }
+
+} )
+
+
+ulFiltros.addEventListener('click' , ( event) => {
+    
+    const filtro = event.target.text;
+    
+    if(!filtro) return;
+    anchorFiltros.forEach( elem => elem.classList.remove('selected')  );
+    event.target.classList.add('selected');
+
+
+    for( const elemento of divTodoList.children){
+
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch (filtro) {
+            case 'Pendientes':
+                if(completado){
+                    elemento.classList.add('hidden')
+                }
+                break;
+            case 'Completados':
+                if(!completado){
+                    elemento.classList.add('hidden')
+                }
+                break;
+        
+            default:
+                break;
+        }
+
+    }
+
+})
